ad96d4d64679b47a60de5ccbda324459c6b54762
SamaraiKai <samuraininjakai@gmail.com>
2026-09-16 10:05:08 -0400

    Update cacheVersion.ts

--- src/cacheVersion.ts
+++ src/cacheVersion.ts
@@ -1,4 +1,4 @@
-export const CACHE_VERSION = "vAfterPilotv5.1";
+export const CACHE_VERSION = "vAfterPilotv5.2";
 
 export const SECTIONS_CACHE_KEY = `bluecatfish_sections_ai_${CACHE_VERSION}`;
 export const AUDIO_FOLDER = `sections_${CACHE_VERSION}`;
